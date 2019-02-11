17db5103b8e6c25069c740a5d0790337c9095499
zhanglei <zhanglei863@crnet.com.hk>
2019-02-11 11:05:56 +0800

    cache1

---
+++ cache1/src/main/java/top/desky/example/redis/cache1/Cache1Application.java
@@ -0,0 +1,14 @@
+package top.desky.example.redis.cache1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cache1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cache1Application.class, args);
+	}
+
+}
+
---
+++ cache1/src/test/java/top/desky/example/redis/cache1/Cache1ApplicationTests.java
@@ -0,0 +1,17 @@
+package top.desky.example.redis.cache1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cache1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
