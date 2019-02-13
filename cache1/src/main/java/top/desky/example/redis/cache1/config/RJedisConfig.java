5dfd674c386a6a2a9abd95e49b83c098b813b06b
zhanglei <zhanglei863@crnet.com.hk>
2019-02-13 15:58:16 +0800

    #10.fine

--- cache1/src/main/java/top/desky/example/redis/cache1/config/RJedisConfig.java
+++ cache1/src/main/java/top/desky/example/redis/cache1/config/RJedisConfig.java
@@ -1,5 +1,7 @@
 package top.desky.example.redis.cache1.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
@@ -11,6 +13,7 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -23,9 +26,25 @@ import java.util.List;
 @EnableCaching
 @Configuration
 public class RJedisConfig extends CachingConfigurerSupport {
+    private static final Logger log = LoggerFactory.getLogger(RJedisConfig.class);
 
     @Autowired
     private RedisConnectionFactory factory;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Bean
+    public void run() {
+        log.info("run==> redis连接工厂：{}", stringRedisTemplate.getConnectionFactory());
+        final String key = "zea";
+
+        //添加key
+        stringRedisTemplate.opsForValue().set(key, "lous");
+        //获取key
+        log.info("run==> 从redis中获取key是{}, value是{}", key, stringRedisTemplate.opsForValue().get(key));
+        //删除key
+        stringRedisTemplate.delete(key);
+    }
 
     @Bean
     public RedisTemplate<String, Serializable> redisTemplate() {
@@ -38,6 +57,12 @@ public class RJedisConfig extends CachingConfigurerSupport {
         return template;
     }
 
+    /**
+     * 跟spring-cache整合时才用到？
+     * 另spring-cache默认用的SimpleCacheConfiguration, 即ConcurrentMapCacheManager方式.
+     *
+     * @return
+     */
     @Bean
     @Override
     public CacheManager cacheManager() {
@@ -60,10 +85,11 @@ public class RJedisConfig extends CachingConfigurerSupport {
     @Bean
     @Override
     public KeyGenerator keyGenerator() {
+        final String SEQ = ":";
         return (target, method, objects) -> {
             StringBuilder sb = new StringBuilder();
-            sb.append(target.getClass().getName());
-            sb.append("::" + method.getName() + ":");
+            sb.append(target.getClass().getName() + SEQ);
+            sb.append(method.getName() + SEQ);
             for (Object obj : objects) {
                 sb.append(obj.toString());
             }
