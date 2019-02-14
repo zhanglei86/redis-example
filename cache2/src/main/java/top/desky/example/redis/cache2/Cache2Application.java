94ef3306bfefa758080462c0e7b9e8c94237854e
zhanglei <zhanglei863@crnet.com.hk>
2019-02-14 14:23:24 +0800

    #18.debug

--- cache2/src/main/java/top/desky/example/redis/cache2/Cache2Application.java
+++ cache2/src/main/java/top/desky/example/redis/cache2/Cache2Application.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Cache2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Cache2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Cache2Application.class, args);
+    }
 
 }
 
